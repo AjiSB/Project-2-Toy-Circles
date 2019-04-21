c9fa135d4041647a5f5cf6ad47e08958323e6ad6
Aji Sohna <7357462+AjiSB@users.noreply.github.com>
2019-04-20 23:24:03 -0700

    added a login page and connected the index, signup and inventory to the backend

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
 var methodOverride = require('method-override')
 
-// https://www.npmjs.com/package/body-parser
 var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
+var path = require('path');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'toycircles_db'
+});
+ 
+connection.connect();
 
 //integrate method override with express
 // override with POST having ?_method=DELETE
-	app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
+app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
 
-app.use(express.static("public"));
+app.get('/', function(req, res) {
+	connection.query('SELECT * FROM toy_inventory', function (error, results, fields) {
+		res.render('index.html', {
+			inventory: results
+		});
+	  });
+});
+
+app.get('/signup.html', function(req, res) {
+	connection.query('SELECT * FROM interests', function (error, results, fields) {
+		res.render('signup.html', {
+			interests: results
+		});
+	});			
+});
+
+
+app.get('/admin_dashboard.html', function(req, res) {
+	res.render('admin_dashboard.html');
+});
+
+app.get('/inventory.html', function(req, res) {
+	connection.query('SELECT * FROM toy_inventory', function (error, results, fields) {
+		res.render('inventory.html', {
+			inventory: results
+		});
+	  });
+});
+
+app.get('/login.html', function(req, res) {
+	res.render('login.html');
+});
+
+//app.use(express.static("public"));
 
 //integrate body-parser with express
 
@@ -20,15 +67,7 @@ app.use(express.static("public"));
 	app.use(bodyParser.json())
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'toycircles_db'
-});
- 
-connection.connect();
+
 
 
 // curl -X GET http://localhost:3001/animals
@@ -40,20 +79,7 @@ app.get('/animals', function(req, res){
 	});
 });
 
-//you can't access a post from the browser
-	/*
-		the only way to access a post route is by using one of the following
 
-			ajax
-
-			a form
-
-			a curl call
-
-				curl -d "animal_name=bengal+tiger" -X POST http://localhost:3001/animals
-
-			chrome extension named post man 
-	*/
 app.post('/animals', function(req, res){
 
 	connection.query('INSERT INTO animals (animal_name) VALUES (?)', [req.body.animal_name],function (error, results, fields) {
